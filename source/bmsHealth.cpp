6dfa223056c775cef5efd58efb9b446617da774a
Bryce Campbell <bwcampbell9@gmail.com>
2020-01-10 19:51:04 -0800

    Implement self test and self test checking.

--- source/bmsHealth.cpp
+++ source/bmsHealth.cpp
@@ -9,8 +9,19 @@ uint16_t _CELLDELTA[12];
 const uint8_t _MD = 2;  // filtered mode
 const uint8_t _DCP = 0; // discharge not permited
 const uint8_t _CH = 0;  // all cells
+const _ADCOPT = 0; // the default value
 
-void commandRDCFGA( void *pvParameters )
+int getSelfTestOutputPatern(uint8_t adcopt, uint8_t md, uint8_t st) {
+    if (md == 1) {
+        if (adcopt == 1) {
+            return st == 1 ? 0x9565 : 0x6A9A;  
+        }
+        return st == 1 ? 0x9553 : 0x6AAC;
+    }
+    return st == 1 ? 0x9555 : 0x6AAA;
+}
+
+void monitorBMSHealth( void *pvParameters )
 {
     TickType_t xLastWakeTime;
     const TickType_t xFrequency = tick_ms(1000);
@@ -20,7 +31,7 @@ void commandRDCFGA( void *pvParameters )
 
     for (;;)
     {
-        int error = pushCommand(RDCFGA, SLAVE_COUNT, RETURN_DATA);
+        // perform diagnostic tests
 
         vTaskDelayUntil( &xLastWakeTime, xFrequency );
     }
@@ -83,10 +94,58 @@ int wiresOpen() {
     return open;
 }
 
-void startSelfTests(uint8_t md, uint8_t st) {
+uint8_t selfTests(uint8_t md, uint8_t st) {
     //DIAGN
     int error = pushCommand(CVST, SLAVE_COUNT, RETURN_DATA, md, st);
     int error = pushCommand(AXST, SLAVE_COUNT, RETURN_DATA, md, st);
     int error = pushCommand(STATST, SLAVE_COUNT, RETURN_DATA, md, st);
 
+    return checkSelfTest(md, st);
 }
+
+/* 
+ * Checs the results of a self test and compares them to the expected results
+ *      given a mode and self test mode
+ * 
+ *  @param md: the ADC mode to use in the check
+ *  @param st: the self test mode to use in the check
+ * 
+ *  @return: a uint8_t with bits set corresponding to error codes fron each self test register group
+ * 
+ * Bits we check:
+ * C1V - C12V (CVA, CVB, CVC, CVD): CVST Result 
+ * G1V - G5V (AUXA, AUXB): AXST Result
+ * SC, ITMP, VA, VD (STATA, STATB): STATST Result
+*/
+uint8_t checkSelfTest(uint8_t md, uint8_t st) {
+    uint8_t expect = getSelfTestOutputPatern(_ADCOPT, md, st);
+
+    uint8_t error = 0;
+
+    int error = pushCommand(RDCVA, SLAVE_COUNT, RETURN_DATA);
+    if (RETURN_DATA != expect) { error |= ERR_CVA; /* SELF TEST FAILED IN CVA */ }
+    int error = pushCommand(RDCVB, SLAVE_COUNT, RETURN_DATA);
+    if (RETURN_DATA != expect) { error |= ERR_CVB; /* SELF TEST FAILED IN CVB */ }
+    int error = pushCommand(RDCVC, SLAVE_COUNT, RETURN_DATA);
+    if (RETURN_DATA != expect) { error |= ERR_CVC; /* SELF TEST FAILED IN CVC */ }
+    int error = pushCommand(RDCVD, SLAVE_COUNT, RETURN_DATA);
+    if (RETURN_DATA != expect) { error |= ERR_CVD; /* SELF TEST FAILED IN CVD */ }
+
+    int error = pushCommand(RDAUXA, SLAVE_COUNT, RETURN_DATA);
+    if (RETURN_DATA != expect) { error |= ERR_AUXA; /* SELF TEST FAILED IN AUXA */ }
+    int error = pushCommand(RDAUXB, SLAVE_COUNT, RETURN_DATA);
+    if (RETURN_DATA != expect) { error |= ERR_AUXB; /* SELF TEST FAILED IN AUXB */ }
+
+    int error = pushCommand(RDSTATA, SLAVE_COUNT, RETURN_DATA);
+    if (RETURN_DATA != expect) { error |= ERR_STATA; /* SELF TEST FAILED IN STATA */ }
+    int error = pushCommand(RDSTATB, SLAVE_COUNT, RETURN_DATA);
+    if (RETURN_DATA != expect) { error |= ERR_STATB; /* SELF TEST FAILED IN STATB */ }
+
+    return error;
+}
+
+/* Other diagnostic data we may want to check:
+ *
+ * MUXFAIL (STATB): Multiplexer self test result
+ * THSD (STATB): Thermal Shutdown Status
+*/
--- source/bmsHealth.h
+++ source/bmsHealth.h
@@ -5,11 +5,11 @@
 #include "bmsCommand.h"
 #include "transaction.h"
 
-// commandRDCFGA: tests read of config reg group A.
-// should read F2-00-00-00-00-00 or similar.
+const uint8 ERR_CVA = 1, ERR_CVB = 2, ERR_CVC = 4, ERR_CVD = 8, ERR_AUXA = 16, ERR_AUXB = 32, ERR_STATA = 64, ERR_STATB = 128;
+
 // to include test task, add to main:
-// xTaskCreate(commandRDCFGA, "rdcfgatest", STACK_SIZE, NULL, 1, NULL );
-void commandRDCFGA(void *pvParameters);
+// xTaskCreate(monitorBMSHealth, "monitorbmshlth", STACK_SIZE, NULL, 1, NULL );
+void monitorBMSHealth(void *pvParameters);
 
 #endif // BMSHEALTH_H
 
