ff6097a3c1ff17a85a08c81e648ebc8f93b378d5
Bryce Campbell <bwcampbell9@gmail.com>
2020-02-21 19:10:55 -0800

    Add can message structure and calculate offsets for data.

--- source/CanMessage.h
+++ source/CanMessage.h
@@ -18,72 +18,83 @@
 #define PWM_MIN 5
 #define PWM_MAX 95
 
+/*
+ * TOTAL CAN ADRESS SPACE REQUIRED: 856.25 Bytes
+ */
+
 /* P0: Evey board sends 32 bit heartbeat(counter) every 10ms that increments after each send 100hz
  */
 
 /* Log current faults and fault history at 10Hz 
  *
- * Faults: 10 Slaves (Max 1040 bits)
- *  Each Slave: (Max 104 bits) 
+ * Faults: (1282 bits) (160.25 Bytes)
+ * 
+ *  Slaves: (up to 12) (Max 48 bits)
  *      (4 bits)
  *      DISCONECTED - No response from slave
  *      CVST
  *      AXST
  *      STATST
  * 
- *      Per Cell: (up to 12) (currently 8) (Max 36 bits)
+ *  Cell Faults: (up to 142) (Max 426 bits)
  *          Overvoltage
  *          Undervoltage
  *          Disconected
  * 
- *      Per Thermistor: (up to 16) (currently 14) (Max 64 bits)
- *          Overtemp
- *          Undertemp
- *          Shorted > 2.9v
+ *  Thermistor Faults: (up to 202) (Max 808 bits)
  *          Disconected < 0.1v
+ *          Shorted > 2.9v
+ *          Undertemp
+ *          Overtemp
+ * 
  */
 
 /* voltage and thermistor 16 bit values every 1hz
+ *
+ *  Cell Values: (Max 2272 Bits)
+ * 
+ *  Thermistor Values: (Max 3232 Bits)
+ *
+ * Thermistors at Cells + 36
+ * Naxt at Thermistors + 51
  */
+
+typedef enum {
+    Heartbeat = 0,
+
+    Slaves = 64,
+    CellFaults = 112,
+    ThermistorFaults = 538,
+    CellValues = 1346, 
+    ThermistorValues = 3618, 
+    SectionEnd = 6850,
+} sectionBitOffsets;
 
 // Each number is the can bit location
 typedef enum{
-    //P: Time in Ms
-    Time = 0,
+    Disconected = 0,
+    FailedCVST,
+    FailedAXST,
+    FailedSTATST
+} SlaveFaultOffsets;
 
-    //P+1: Faults
-    Apps10InfoFault = 64,
-    Apps10NonCriticalFault,
-    Apps1OvervoltageInfo,
-    Apps1OvervoltageNonCritical,
-    Apps2OvervoltageInfo,
-    Apps2OvervoltageNonCritical,
-    BrakePressure1SoftwareOvervoltageInfo,
-    BrakePressure1SoftwareOvervoltageNonCritical,
+typedef enum{
+    Undervoltage = 0,
+    Overvoltage,
+    Disconected,
+} CellFaultOffsets;
 
-    //P+3: Apps, Current
+typedef enum{
+    Disconected = 0,
+    Shorted,
+    Undertemp,
+    Overtemp,
+} ThermistorFaultOffsets;
 
-    Apps1Data = 64 * 3,
-    Apps2Data = (64 * 3) + 16,
-    CurrentThrottle1Data = (64 * 3) + 32,
-    CurrentThrottle2Data = (64 * 3) + 48,
-
-    //P+4: Brake
-    BrakePressure1Data = 64 * 4,
-    BrakePressure2Data = (64 * 4) + 16,
-    BrakePosition1Data = 64 * 4 + 32,
-    BrakePosition2Data = (64 * 4) + 48,
-
-    NULLVAL = 10000
-
-
-} BitLocation;
-
-union Frame{
-    uint64_t faults;
-    uint8_t faultsArray[8];
-    uint16_t dataArray[4];
-};
+uint64_t heartbeat;
+uint8_t faultsCurrent[161];
+uint8_t faultsHistory[161];
+uint16_t dataArray[688];
 
 class CanMessage{
     public:
@@ -93,6 +104,11 @@ class CanMessage{
         void set16(BitLocation bl, uint16_t data);
         void set64(BitLocation bl, uint16_t data);
         void fullSend();
+        int getSlaveOffset(int slaveNumber) { return Slaves + (slaveNumber * 4) }
+        int getCellFaultOffset(int cellNumber) { return CellFaults + (cellNumber * 3) }
+        int getThermistorFaultOffset(int thermistorNumber) { return ThermistorFaults + (thermistorNumber * 4) }
+        int getCellValueOffset(int cellNumber) { return CellValues + (cellNumber * 16)}
+        int getThermistorValueOffset(int thermistorNumber) { return ThermistorValues + (thermistorNumber * 16) }
     private:
         union Frame frame[5];
         static CanMessage* instance;
